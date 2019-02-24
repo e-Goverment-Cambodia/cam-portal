83433ba27538a12dffd05d2f8adf0f35444d5f4b
Sovichetra <chetra-chann@mptc.gov.kh>
2019-02-24 18:41:54 +0700

    Archived and Page template

--- archive.php
+++ archive.php
@@ -9,30 +9,44 @@
 
 get_header();
 ?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+	<div class="container">
+		<div class="breadcrum">
+			<ul>
+			<?php
+				the_archive_title('<li><a href="#">','</a></li>');
+			?>
+			</ul>
+		</div>
+	</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8">
+					<div class="b-2">
 
 		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
 
 			<?php
 			/* Start the Loop */
 			while ( have_posts() ) :
 				the_post();
-
+				?>
+			<div class="b-item-wrap">
+					<div class="b-item row">
+						<div class="b-thumnail-wrap col-5">
+							<div class="b-thumnail"><?php cam_portal_the_post_thumbnail(); ?></div>
+						</div>
+						<div class="b-title-wrap col-7">
+							<div class="b-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
+							<div class="b-date"><?php  cam_portal_posted_on(); ?></div>
+						</div>
+					</div>
+				</div>
+				<?php
 				/*
 				 * Include the Post-Type-specific template for the content.
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', get_post_type() );
 
 			endwhile;
 
@@ -45,9 +59,13 @@ get_header();
 		endif;
 		?>
 
-		</main><!-- #main -->
+			</div>
+		</div>
+			<?php
+			get_sidebar();
+			?>
+		</div><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
